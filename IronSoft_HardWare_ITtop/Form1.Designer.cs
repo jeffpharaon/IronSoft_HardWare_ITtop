205ed063891214e4bef8c2e4807a3a3812a6a4f3
jeffp <jeffp@JEFFPHARAON>
2024-07-27 19:27:39 +0300

    Добавьте файлы проекта.

---
+++ IronSoft_HardWare_ITtop/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace IronSoft_HardWare_ITtop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Items.AddRange(new object[] {
+            "CPU",
+            "Chipset",
+            "Battery",
+            "BIOS",
+            "RAM",
+            "Cache",
+            "USB",
+            "Disk",
+            "Logical Disks",
+            "Network",
+            "Users"});
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 64);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 374);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "NAME";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "MEANING";
+            this.columnHeader2.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "IronSoft HardWare";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ IronSoft_HardWare_ITtop/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Management;
+
+namespace IronSoft_HardWare_ITtop
+{
+    public partial class Form1 : Form
+    {
+        private string key = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) => toolStripComboBox1.SelectedIndex = 0;
+
+        private void GetInfoHardWare(string key, ListView listView)
+        {
+            listView.Items.Clear();
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM " + key);  
+
+            try
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    ListViewGroup group;
+
+                    try
+                    {
+                        group = listView.Groups.Add(obj["Name"].ToString(), obj["Name"].ToString());
+                    }
+                    catch
+                    {
+                        group = listView.Groups.Add(obj.ToString(), obj.ToString());
+                    }
+
+                    if (obj.Properties.Count == 0) 
+                    {
+                        MessageBox.Show("Failed to receive information", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    foreach (PropertyData property in obj.Properties)
+                    {
+                        ListViewItem item = new ListViewItem(group);
+
+                        if (listView.Items.Count % 2 != 0) item.BackColor = Color.White;
+                        else item.BackColor = Color.WhiteSmoke;
+
+                        item.Text = property.Name;
+
+                        if (property.Value != null && !string.IsNullOrEmpty(property.Value.ToString()))
+                        {
+                            switch (property.Value.GetType().ToString())
+                            {
+                                case "System.String[]":
+                                    string[] stringData = property.Value as string[];
+                                    string strOne = string.Empty;
+
+                                    foreach (string str in stringData)
+                                        strOne += $"{str}";
+
+                                    item.SubItems.Add(strOne);
+                                    break;
+
+                                case "System.UInt16[]":
+                                    ushort[] ushortData = property.Value as ushort[];   
+                                    string strTwo = string.Empty;
+
+                                    foreach (var usr in ushortData)
+                                        strTwo += $"{Convert.ToString(usr)}";
+
+                                    item.SubItems.Add(strTwo);
+                                    break;
+
+                                default:
+                                    item.SubItems.Add(property.Value.ToString());
+                                    break;
+                            }
+
+                            listView.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (toolStripComboBox1.SelectedItem.ToString()) 
+            {
+                case "CPU":
+                    key = "Win32_Processor";
+                    break;
+
+                case "GPU":
+                    key = "Win32_VideoController";
+                    break;
+
+                case "Сhipset":
+                    key = "Win32_IDEController";
+                    break;
+
+                case "Battery":
+                    key = "Win32_Battery";
+                    break;
+
+                case "BIOS":
+                    key = "Win32_BIOS";
+                    break;
+
+                case "RAM":
+                    key = "Win32_PhysicalMemory";
+                    break;
+
+                case "Cache":
+                    key = "Win32_CacheMemory";
+                    break;
+
+                case "USB":
+                    key = "Win32_USBController";
+                    break;
+
+                case "Disk":
+                    key = "Win32_DiskDrive";
+                    break;
+
+                case "Logical Disks":
+                    key = "Win32_LogicalDisk";
+                    break;
+
+                case "Keyboard":
+                    key = "Win32_Keyboard";
+                    break;
+
+                case "Network":
+                    key = "Win32_NetworkAdapter";
+                    break;
+
+                case "Users":
+                    key = "Win32_Account";
+                    break;
+
+                default:
+                    key = "Win32_Processor";
+                    break;
+            }
+
+            GetInfoHardWare(key, listView1);
+        }
+    }
+}
